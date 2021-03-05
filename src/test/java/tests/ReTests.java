90b9c478c6e429d5483eea2ed1e4a1b86536614b
Sonal Agarwal <sonal.fd@gmail.com>
2021-03-05 22:10:59 +0530

    Added ReTests.java

---
+++ src/test/java/tests/ReTests.java
@@ -0,0 +1,52 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utils.ExtentReports.ExtentTestManager;
+
+import java.lang.reflect.Method;
+
+public class ReTests extends BaseTest {
+
+    // Extra information:
+    // 1) @BeforeClass we declared driver variable.
+    // 2) We send driver variable to the page class with below declaration.
+    //    Homepage homepage = new HomePage(driver);
+    // 3) super () method in page class transfer the driver variable value to the BasePage class.
+
+    @Test(priority = 0, description = "Invalid Login Scenario with wrong username and password.")
+    public void invalidLoginTest_InvalidUserNameInvalidPassword(Method method) {
+        //ExtentReports Description
+        ExtentTestManager.startTest(method.getName(), "Invalid Login Scenario with invalid username and password.");
+
+        homePage
+            .goToN11()
+            .goToLoginPage()
+            .loginToN11("asdasdas", "111")
+            .verifyLoginUserName("Lütfen geçerli bir e-posta adresi girin.")
+            .verifyLoginPassword("Girilen değer en az 6 karakter olmalıdır.");
+    }
+
+    @Test(priority = 0, description = "Invalid Login Scenario with wrong username and password.")
+    public void testMethod(Method method) {
+        //ExtentReports Description
+        ExtentTestManager.startTest(method.getName(), "Navigating to login page");
+
+        homePage
+                .goToN11()
+                .goToLoginPage();
+
+    }
+
+  /*  @Test(priority = 1, description = "Invalid Login Scenario with empty username and password.")
+    public void invalidLoginTest_EmptyUserEmptyPassword(Method method) {
+        //ExtentReports Description
+        ExtentTestManager.startTest(method.getName(), "Invalid Login Scenario with empty username and password.");
+
+        homePage
+            .goToN11()
+            .goToLoginPage()
+            .loginToN11("", "")
+            .verifyLoginUserName("Lütfen e-posta adresinizi girin.")
+            .verifyLoginPassword("WRONG MESSAGE FOR FAILURE!");
+    }*/
+}
