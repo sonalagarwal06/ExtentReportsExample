6bb5920d8d9d546234c2ba76072ea43d894e3fd6
Sonal Agarwal <sonal.fd@gmail.com>
2021-03-05 21:16:31 +0530

    Commenting two methods

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -12,7 +12,7 @@ public class LoginTests extends BaseTest {
     //    Homepage homepage = new HomePage(driver);
     // 3) super () method in page class transfer the driver variable value to the BasePage class.
 
-    @Test(priority = 0, description = "Invalid Login Scenario with wrong username and password.")
+   /* @Test(priority = 0, description = "Invalid Login Scenario with wrong username and password.")
     public void invalidLoginTest_InvalidUserNameInvalidPassword(Method method) {
         //ExtentReports Description
         ExtentTestManager.startTest(method.getName(), "Invalid Login Scenario with invalid username and password.");
@@ -23,9 +23,20 @@ public class LoginTests extends BaseTest {
             .loginToN11("asdasdas", "111")
             .verifyLoginUserName("Lütfen geçerli bir e-posta adresi girin.")
             .verifyLoginPassword("Girilen değer en az 6 karakter olmalıdır.");
+    }*/
+
+    @Test(priority = 0, description = "Invalid Login Scenario with wrong username and password.")
+    public void testMethod(Method method) {
+        //ExtentReports Description
+        ExtentTestManager.startTest(method.getName(), "Navigating to login page");
+
+        homePage
+                .goToN11()
+                .goToLoginPage();
+
     }
 
-    @Test(priority = 1, description = "Invalid Login Scenario with empty username and password.")
+  /*  @Test(priority = 1, description = "Invalid Login Scenario with empty username and password.")
     public void invalidLoginTest_EmptyUserEmptyPassword(Method method) {
         //ExtentReports Description
         ExtentTestManager.startTest(method.getName(), "Invalid Login Scenario with empty username and password.");
@@ -36,5 +47,5 @@ public class LoginTests extends BaseTest {
             .loginToN11("", "")
             .verifyLoginUserName("Lütfen e-posta adresinizi girin.")
             .verifyLoginPassword("WRONG MESSAGE FOR FAILURE!");
-    }
+    }*/
 }
